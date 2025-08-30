37c54a121a1ee1ef36f13252744043fb47f5b41d
n.akbari <n.akbari@mohaymen.ir>
2025-08-30 11:47:49 +0330

    modify

--- restassuredAuura/src/test/java/ir/sample/TestSample.java
+++ restassuredAuura/src/test/java/ir/sample/TestSample.java
@@ -14,6 +14,21 @@ public class TestSample {
     @Story("should be failed2")
     @Severity(SeverityLevel.NORMAL)
     @Description("Test Description : i want to test2")
+    public void FailedAPISample1() {
+        given()
+                .filter(new AllureRestAssured())
+                .baseUri("https://jsonplaceholder.typicode.com")
+                .header("Content-Type", "application/json")
+                .when()
+                .get("/posts/1")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test(description = "failed Test2", priority = 1)
+    @Story("should be failed2")
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Test Description : i want to test2")
     public void FailedAPISample() {
         given()
                 .filter(new AllureRestAssured())
